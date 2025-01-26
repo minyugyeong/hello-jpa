2b2438c0fd3f072f94ccc23b8586a27a57367476
minyugyeong <alsdbrud123@naver.com>
2025-01-26 17:06:05 +0900

    섹션5. 엔티티 매핑 - 객체와 테이블 매핑

---
+++ src/main/java/hellojpa_section5/Member.java
@@ -0,0 +1,36 @@
+package hellojpa_section5;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "MBR")
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
