3f884337a2bc383620d900bfa1e27f0f76eb64e4
minyugyeong <alsdbrud123@naver.com>
2025-01-26 17:17:52 +0900

    섹션5. 엔티티 매핑 - 데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa_section5/Member.java
+++ src/main/java/hellojpa_section5/Member.java
@@ -1,13 +1,18 @@
 package hellojpa_section5;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "MBR")
 public class Member {
 
+    @Id
     private Long id;
+
+    @Column(unique = true, length = 10) //DDL 자동 생성할 때만 사용되고, JPA 실행 로직에는 영향 X
     private String name;
 
     public Member() {
